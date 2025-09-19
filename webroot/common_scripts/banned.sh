34b08e6afa417099f29dc42ef9262671f7ea3f7e
MONA <121730102+MeowDump@users.noreply.github.com>
2025-09-20 01:06:02 +0530

    ‎

--- webroot/common_scripts/banned.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-nohup am start -a android.intent.action.VIEW -d https://android.googleapis.com/attestation/status >/dev/null 2>&1 &
-echo "Redirecting to Google Revoked List"
-exit 0
