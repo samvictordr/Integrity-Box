2f42705e888713849b2218c729ca8987450eebce
MONA <121730102+MeowDump@users.noreply.github.com>
2025-09-20 01:10:10 +0530

    ‎

--- webroot/common_scripts/support.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-popup() {
-    am start -a android.intent.action.MAIN -e mona "$@" -n meow.helper/.MainActivity &>/dev/null
-    sleep 0.5
-}
-
-popup "This feature is under maintenance"
-exit 0
