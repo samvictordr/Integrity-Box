0c01130f025955ad6a220feafab5ed68b5aaa275
MONA <121730102+MeowDump@users.noreply.github.com>
2025-11-05 14:29:14 +0000

    Adapted scripts-only-mode from PIF by @osm0sis
    Signed-off-by: MONA <121730102+MeowDump@users.noreply.github.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -291,4 +291,42 @@ if [ -d "/data/adb/magisk" ]; then
 #    add_if_missing com.google.android.gsf com.google.process.gservices
 fi
 
+##########################################
+# adapted from Play Integrity Fork by @osm0sis
+# source: https://github.com/osm0sis/PlayIntegrityFork
+# license: GPL-3.0
+##########################################
+
+# Conditional early sensitive properties
+# Samsung
+resetprop_if_diff ro.boot.warranty_bit 0
+resetprop_if_diff ro.vendor.boot.warranty_bit 0
+resetprop_if_diff ro.vendor.warranty_bit 0
+resetprop_if_diff ro.warranty_bit 0
+
+# Realme
+resetprop_if_diff ro.boot.realmebootstate green
+
+# OnePlus
+resetprop_if_diff ro.is_ever_orange 0
+
+# Microsoft
+for PROP in $(resetprop | grep -oE 'ro.*.build.tags'); do
+    resetprop_if_diff $PROP release-keys
+done
+
+# Other
+for PROP in $(resetprop | grep -oE 'ro.*.build.type'); do
+    resetprop_if_diff $PROP user
+done
+resetprop_if_diff ro.adb.secure 1
+if ! $SKIPDELPROP; then
+    delprop_if_exist ro.boot.verifiedbooterror
+    delprop_if_exist ro.boot.verifyerrorpart
+fi
+resetprop_if_diff ro.boot.veritymode.managed yes
+resetprop_if_diff ro.debuggable 0
+resetprop_if_diff ro.force.debuggable 0
+resetprop_if_diff ro.secure 1
+
 exit 0
